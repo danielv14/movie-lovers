0cadc57a511893c8ab2bd447bfdb08157c1189e5
Daniel <daniel.vernberg@gmail.com>
2015-04-15 10:10:07 +0200

    normalize.css is in place. navbar is in place

--- header.php
+++ header.php
@@ -3,7 +3,22 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Min sida</title>
+	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	
+
+<!-- Navigation goes here -->
+<div id="navigation">
+	<a href="#">Start</a>
+	<a href="#">Länk 1</a>
+	<a href="#">Länk 2</a>
+	<a href="#">Länk 3</a>
+	<a href="#">Länk 4</a>
+	<a href="#">Länk 5</a>	
+
+
+
+
+</div>	
