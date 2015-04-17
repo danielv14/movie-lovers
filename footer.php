fc2435b1fd2cc2f8ecf68e9393e5b0ffa8ae4fba
Daniel <daniel.vernberg@gmail.com>
2015-04-17 21:28:24 +0200

    Added js/parallax.js and linked it in the footer

--- footer.php
+++ footer.php
@@ -2,6 +2,8 @@
 <script src="js/jquery-2.1.3.min.js"></script>
 <script src="js/wow.min.js" type="text/javascript"></script>
 <script src="js/main.js" type="text/javascript"></script>
+<script src="js/parallax.min.js" type="text/javascript"></script>
+
 <script>
  new WOW().init();
 </script>
